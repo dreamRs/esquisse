62b98ab5f2c4502d0b8a3f5e37c326c7a295ca5f
pvictor <perrier.victor@gmail.com>
2018-10-07 18:40:56 +0200

    examples for esquisse as module

--- R/esquisserUI.R
+++ R/esquisserUI.R
@@ -5,6 +5,8 @@
 #' @param id Module's id.
 #' @param header Logical. Display or not \code{esquisse} header.
 #' @param choose_data Logical. Display or not the button to choose data.
+#' 
+#' @note For the module to display correctly, it is necessary to place it in a container with a fixed height.
 #'
 #' @export
 #' 
@@ -14,6 +16,87 @@
 #' @importFrom shiny plotOutput icon actionButton NS
 #' @importFrom miniUI miniTitleBarButton miniPage
 #'
+#' @examples 
+#' 
+#' if (interactive()) {
+#' 
+#' 
+#' ### Part of a Shiny app ###
+#' 
+#' library(shiny)
+#' library(esquisse)
+#' 
+#' ui <- fluidPage(
+#'   tags$h1("Use esquisse as a Shiny module"),
+#'   radioButtons(
+#'     inputId = "data", 
+#'     label = "Data to use:", 
+#'     choices = c("iris", "mtcars"),
+#'     inline = TRUE
+#'   ),
+#'   tags$div(
+#'     style = "height: 700px;", # needs to be in fixed height container
+#'     esquisserUI(
+#'       id = "esquisse", 
+#'       header = FALSE, # dont display gadget title
+#'       choose_data = FALSE # dont display button to change data
+#'     )
+#'   )
+#' )
+#' 
+#' server <- function(input, output, session) {
+#'   
+#'   data_r <- reactiveValues(data = iris, name = "iris")
+#'   
+#'   observeEvent(input$data, {
+#'     if (input$data == "iris") {
+#'       data_r$data <- iris
+#'       data_r$name <- "iris"
+#'     } else {
+#'       data_r$data <- mtcars
+#'       data_r$name <- "mtcars"
+#'     }
+#'   })
+#'   
+#'   callModule(module = esquisserServer, id = "esquisse", data = data_r)
+#'   
+#' }
+#' 
+#' shinyApp(ui, server)
+#' 
+#' 
+#' 
+#' ### Whole Shiny app ###
+#' 
+#' library(shiny)
+#' library(esquisse)
+#' 
+#' 
+#' # Load some datasets in app environment
+#' my_data <- data.frame(
+#'   var1 = rnorm(100),
+#'   var2 = sample(letters[1:5], 100, TRUE)
+#' )
+#' 
+#' 
+#' 
+#' ui <- fluidPage(
+#'   tags$div( # needs to be in fixed height container
+#'     style = "position: fixed; top: 0; bottom: 0; right: 0; left: 0;", 
+#'     esquisserUI(id = "esquisse")
+#'   )
+#' )
+#' 
+#' server <- function(input, output, session) {
+#'   
+#'   callModule(module = esquisserServer, id = "esquisse")
+#'   
+#' }
+#' 
+#' shinyApp(ui, server)
+#' 
+#' }
+#' 
 esquisserUI <- function(id, header = TRUE, choose_data = TRUE) {
   
   ns <- NS(id)
