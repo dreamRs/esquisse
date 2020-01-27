5b2b5c9176c4c118828bc593611b945d0203edbc
xiangnan dang <c242469@rstudio0003.am.lilly.com>
2020-01-27 11:43:42 -0500

    facet nrow

--- R/module-chartControls.R
+++ R/module-chartControls.R
@@ -563,7 +563,7 @@ controls_params <- function(ns) {
     tags$div(
       id = ns("controls-facet-nrow"), style = "display: none;",
       sliderInput(
-        inputID = ns("facet_nrow"),
+        inputId = ns("facet_nrow"),
         label = "Facet nrow:",
         min = 1, max = 10,
         value = 1
