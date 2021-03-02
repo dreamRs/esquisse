acd1c2b1be88359ad2d4048cfef85ef3bcb1045f
pvictor <perrier.victor@gmail.com>
2021-03-02 19:08:19 +0100

    updated resizer handler js

--- inst/assets/moveable/resizer-handler.js
+++ inst/assets/moveable/resizer-handler.js
@@ -1,13 +1,18 @@
+/*jshint
+  jquery:true,
+  devel: true
+*/
+/*global Shiny, Moveable */
 $(function() {
   Shiny.addCustomMessageHandler("resize", function(obj) {
-
     var el = document.getElementById(obj.id);
     if (el === null) {
-      consol.log("Resize : element not found");
+      console.log("Resize : element not found");
       return;
     }
 
-    const moveable = new Moveable(document.querySelector(obj.container), {
+    var widthModal = 900;
+    var moveable = new Moveable(document.querySelector(obj.container), {
       //document.body //
       target: el,
       resizable: true,
@@ -20,7 +25,7 @@ $(function() {
       padding: { left: 0, top: 0, right: 0, bottom: 0 }
     });
 
-    let frame = {
+    var frame = {
       translate: [0, 0]
     };
     moveable
@@ -29,39 +34,46 @@ $(function() {
         e.dragStart && e.dragStart.set(frame.translate);
       })
       .on("resize", function(e) {
-        const beforeTranslate = e.drag.beforeTranslate;
+        var beforeTranslate = e.drag.beforeTranslate;
 
         frame.translate = beforeTranslate;
         e.target.style.width = e.width + "px";
         e.target.style.height = e.height + "px";
-        e.target.style.transform = `translate(${beforeTranslate[0]}px, ${
-          beforeTranslate[1]
-        }px)`;
-
-        var browserWidth = window.innerWidth
-            || document.documentElement.clientWidth
-            || document.body.clientWidth;
+        e.target.style.transform = "translate("
+          .concat(beforeTranslate[0], "px, ")
+          .concat(beforeTranslate[1], "px)");
+        var browserWidth =
+          window.innerWidth ||
+          document.documentElement.clientWidth ||
+          document.body.clientWidth;
 
         $("#" + obj.id).trigger("resize");
         if (obj.modal) {
-          var widthModal = $(".modal-dialog").width();
+          if ($(".modal-dialog").width() < 900) {
+            widthModal = $(".modal-dialog").width();
+          }
           if (e.width > widthModal) {
             if (e.width > browserWidth) {
               moveable.resizable = false;
               $(".modal-dialog").width(browserWidth - 40);
-              $("#" + obj.id).parent().css("overflow-x", "scroll");
+              $("#" + obj.id)
+                .parent()
+                .css("overflow-x", "scroll");
             } else {
               moveable.resizable = true;
               $(".modal-dialog").width(e.width + 30);
-              $("#" + obj.id).parent().css("overflow-x", "hidden");
+              $("#" + obj.id)
+                .parent()
+                .css("overflow-x", "hidden");
             }
           } else {
             moveable.resizable = true;
             $(".modal-dialog").width(widthModal);
-            $("#" + obj.id).parent().css("overflow-x", "hidden");
+            $("#" + obj.id)
+              .parent()
+              .css("overflow-x", "hidden");
           }
         }
-
       });
 
     Shiny.addCustomMessageHandler("resize-" + obj.id, function(obj) {
