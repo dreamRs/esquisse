5861cfeb0b4d3506b3dd377d31675e540d17a9ba
pvictor <victor.perrier@dreamRs.fr>
2022-09-23 13:44:17 +0200

    variables badges: custom class for label

--- R/utils.R
+++ R/utils.R
@@ -133,15 +133,15 @@ badgeType <- function(col_name, col_type) {
       col_name_i <- col_name[i]
       col_type_i <- col_type[i]
       if (col_type_i == "discrete") {
-        tags$span(class = "label label-discrete badge-dad", col_name_i)
+        tags$span(class = "label-esquisse label-discrete badge-dad", col_name_i)
       } else if (col_type_i == "time") {
-        tags$span(class = "label label-datetime badge-dad", col_name_i)
+        tags$span(class = "label-esquisse label-datetime badge-dad", col_name_i)
       } else if (col_type_i == "continuous") {
-        tags$span(class="label label-continuous badge-dad", col_name_i)
+        tags$span(class="label-esquisse label-continuous badge-dad", col_name_i)
       } else if (col_type_i == "id") {
-        tags$span(class = "label label-default badge-dad", col_name_i)
+        tags$span(class = "label-esquisse label-default badge-dad", col_name_i)
       } else {
-        tags$span(class = "label label-other badge-dad", col_name_i)
+        tags$span(class = "label-esquisse label-other badge-dad", col_name_i)
       }
     }
   )
